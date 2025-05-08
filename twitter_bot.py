d34439abb22bdb00af2783a0353b83f99c1966b1
Alfred <alfrednimelymwc@gmail.com>
2025-05-08 18:25:43 -0400

    Decided to separate the discord code and twitter code into two different files for easier readability.

---
+++ twitter_bot.py
@@ -0,0 +1,9 @@
+import tweepy
+import logging
+
+# Start of twitter code
+
+client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAMda0wEAAAAAbehbtd1ZMqM8dAVfkhlu3xlx9dk%3DhpiS6TooWhPTDjqGzvEh2eQOPI2rCeTioS9sEqlzczZkovApqs")
+logging.basicConfig(level=logging.DEBUG)
+
+# End of twitter code
