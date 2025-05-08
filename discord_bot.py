e8a5077aeff76d579f5843ac6951b3727a6b71b6
Alfred <alfrednimelymwc@gmail.com>
2025-05-08 18:25:22 -0400

    Decided to separate the discord code and twitter code into two different files for easier readability.

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,4 @@
 import discord
-import tweepy
 
 # Start of discord code
 
@@ -23,9 +22,3 @@ async def on_message(message):
 client.run('MTI4MzY4MDQ3MzMzNTQ2Mzk3MA.G9Gxaq.gZ0LLf4GSTGLgym8KVWA0bJolZoJ-vCFwB3l5I')
 
 # End of discord code
-
-# Start of twitter code
-
-client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAMda0wEAAAAAbehbtd1ZMqM8dAVfkhlu3xlx9dk%3DhpiS6TooWhPTDjqGzvEh2eQOPI2rCeTioS9sEqlzczZkovApqs")
-
-# End of twitter codes
